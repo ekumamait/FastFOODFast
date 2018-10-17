2019943764d5caa7256fcab22b062ef189ac711c
ekumamait <ericebuluochol@gmail.com>
2018-10-17 18:56:02 +0300

    fix travis

--- tests/test_promote_user.py
+++ tests/test_promote_user.py
@@ -4,21 +4,11 @@ from app import app
 from app.models import Database
 from app.models import Users, Orders, Menu
 
-class My_TestClass(unittest.TestCase):
 
-    def __init__(self):
-        self.app = app.test_client() 
+def test_promote_user():
+    """tests for promote up a user"""
+    test_app = app.test_client()
+    response =  test_app.put('/api/v2/users/20')
+    assert response.status_code == 201
+    assert response.content_type == 'application/json' 
 
-    def setUp(self):
-        self.dbcon = Database()
-        self.dbcon.table()
-
-    def test_promote_user(self):
-        """tests for promote up a user"""
-        response =  self.app.put('/api/v2/users/20')
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(response.content_type, 'application/json') 
-    
-    def tearDown(self):
-        db = Database()
-        db.drop_tables()
