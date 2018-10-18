8ef9d1d9b6967c670f1f2aed52f2e1fc947bf2b2
ekumamait <ericebuluochol@gmail.com>
2018-10-18 14:26:04 +0300

    add js templates, add flask-cors to requirements and modify __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 """ Api endpoint logic """
 
 from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
 from flask_jwt_extended import JWTManager, jwt_required, create_access_token,get_jwt_identity 
 
 app = Flask(__name__)
+CORS(app) 
 
 
 app.config['JWT_SECRET_KEY'] = 'super-secret'
